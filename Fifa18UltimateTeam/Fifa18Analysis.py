8ad7a8d99c18345cc96eb6c3826f584eed6ee69e
Hunter Somers <42579246+hsomers@users.noreply.github.com>
2018-10-17 13:26:16 -0500

    Add files via upload

--- Fifa18UltimateTeam/Fifa18Analysis.py
+++ Fifa18UltimateTeam/Fifa18Analysis.py
@@ -10,27 +10,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 imp = pd.read_csv('FIFA18 - Ultimate Team players.csv', header = 0)
-
 df = pd.DataFrame(imp)
 
 #Question: Who are the top-rated players at each position?
-df.loc[df['cb'].idxmax()]
 
 #Create mini dataframes with all the top players in each position
-CBs = df[df['cb']==df['cb'].max()][['player_name','position','cb']]
-RBs = df[df['rb']==df['rb'].max()][['player_name','position','rb']]
-LBs = df[df['lb']==df['lb'].max()][['player_name','position','lb']]
-CDMs = df[df['cdm']==df['cdm'].max()][['player_name','position','cdm']]
-CMs = df[df['cm']==df['cm'].max()][['player_name','position','cm']]
-RWs = df[df['rw']==df['rw'].max()][['player_name','position','rw']]
-LWs = df[df['lw']==df['lw'].max()][['player_name','position','lw']]
-STs = df[df['st']==df['st'].max()][['player_name','position','st']]
-
 def Find_Top_At_Pos(df, Pos):
     return df[df[Pos]==df[Pos].max()][['player_name','position',Pos]]
 
+CBs = Find_Top_At_Pos(df, 'cb')
+RBs = Find_Top_At_Pos(df, 'rb')
+LBs = Find_Top_At_Pos(df, 'lb')
+CDMs = Find_Top_At_Pos(df, 'cdm')
+CMs = Find_Top_At_Pos(df, 'cm')
+LWs = Find_Top_At_Pos(df, 'lw')
+RWs = Find_Top_At_Pos(df, 'rw')
+STs = Find_Top_At_Pos(df, 'st')
+
+#I need some different mechanism for gk because it doesn't have its own column
+
+
+
+#Now let's select the top_n for each position that we need
+"""Want 4-3-3
+        1 gk
+        2 cb
+        1 rb
+        1 lb
+        1 cdm
+        2 cm
+        1 lw
+        1 rw
+        1 st
+"""
 def Select_Top1_At_Pos(Plyrs,Pos):
     return Plyrs.iloc[0][['player_name',Pos]]
 
-CBs = Find_Top_At_Pos(df, 'cb')
-CB1 = Select_Top1_At_Pos(CBs, 'cb')
+
+
+
